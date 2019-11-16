1d6ce3372c31974a9c5d3997d5064300b8c57a81
John Thielman <john.thielman@gmail.com>
2019-11-16 11:48:11 -0600

    apiKeys and started auth

--- src/javascripts/components/Auth/auth.js
+++ src/javascripts/components/Auth/auth.js
@@ -1,2 +1,21 @@
-// import firebase from 'firebase/app';
-// import utilities from '../../helpers/utilities';
+import $ from 'jquery';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import googlebutton from './googlebutton.png';
+import utilities from '../../helpers/utilities';
+
+const signMeIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+};
+
+const loginButton = () => {
+  const domString = `<button id="google-auth" class="btn btn-success">
+  <img src="${googlebutton}" />
+  </button>`;
+  utilities.printToDom('auth', domString);
+  $('#google-auth').click(signMeIn);
+};
+
+export default { loginButton };
