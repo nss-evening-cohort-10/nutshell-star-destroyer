14433f9dd0ea3afe3184d90a4c518f7b54a56526
Emilee Mitchell <emcakes@gmail.com>
2019-11-18 16:58:15 -0600

    Cards are printing

--- src/javascripts/components/Personnel/personnel.js
+++ src/javascripts/components/Personnel/personnel.js
@@ -4,28 +4,29 @@ import personnelData from '../../helpers/data/personnelData';
 import utilities from '../../helpers/utilities';
 
 
-const displayCrew = () => {
-  personnelData.personnelDataByPersonId('person4')
+const displayCrew = (e) => {
+  e.preventDefault();
+  personnelData.getPersonnelData()
     .then((personnel) => {
       console.log(personnel);
-      let domString = `
-      <h1>Personnel</h1>
-      `;
+      let domString = '<h1>Personnel</h1>';
+      domString += '<div class="row">';
       personnel.forEach((person) => {
         domString += `
         <div class="col-sm-4">
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title">Name ${person.name}</h5>
-              <p class="card-text">Sector ${person.sector}</p>
-              <p class="card-text">Weapon: ${person.weapon}</p>
+              <h5 class="card-title">Name: ${person.name}</h5>
+              <p class="card-text">Sector: ${person.sectorId}</p>
+              <p class="card-text">Weapon: ${person.weaponId}</p>
             </div>
           </div>
         </div>
         `;
       });
       domString += '</div>';
-      utilities.printToDom('printComponent', domString);
+      console.log(domString);
+      utilities.printToDom('#personnel', domString);
     })
     .catch((error) => console.error(error));
 };
--- src/javascripts/helpers/data/personnelData.js
+++ src/javascripts/helpers/data/personnelData.js
@@ -4,15 +4,15 @@ import 'firebase/auth';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
-const personnelDataByPersonId = (personId) => new Promise((resolve, reject) => {
-  axios.get(`${baseUrl}/personnel.json?orderBy="personId"&equalTo="${personId}"`)
+const getPersonnelData = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/personnel.json`)
     .then((response) => {
       const demPersonnel = response.data;
       const personnel = [];
-      Object.keys(demPersonnel).forEach((pId) => {
-        demPersonnel[pId].id = pId;
+      Object.keys(demPersonnel).forEach((personId) => {
+        demPersonnel[personId].id = personId;
         // let's make a new id on it called pId and shove it in there. Then we're pushing
-        personnel.push(demPersonnel[pId]);
+        personnel.push(demPersonnel[personId]);
         // this should now give you back an array with one person
       });
 
@@ -21,4 +21,4 @@ const personnelDataByPersonId = (personId) => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
-export default { personnelDataByPersonId };
+export default { getPersonnelData };
