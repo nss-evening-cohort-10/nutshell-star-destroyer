c9d232948ed2b74a6a34386fbe4093b3f9476db3
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-11-18 19:41:47 -0600

    created functions for adding new enemy

--- src/javascripts/components/Enemies/enemies.js
+++ src/javascripts/components/Enemies/enemies.js
@@ -8,16 +8,39 @@ import utilities from '../../helpers/utilities';
 // console.log(empireEnemies);
 // };
 
+const addNewEnemy = (e) => {
+  e.stopImmediatePropagation();
+  const newEnemy = {
+    name: $('#enemyName').val(),
+    imageUrl: $('#enemyImage').val(),
+    baseSector: $('#enemySector').val(),
+    LKL: $('#enemyLKL').val(),
+    isDead: $('#enemyDead').val(),
+    isCaptured: $('#enemyCaptured').val(),
+  };
+  console.log(newEnemy);
+  enemyData.addEnemy(newEnemy)
+    .then(() => {
+      $('#exampleModal').modal('hide');
+      // eslint-disable-next-line no-use-before-define
+      enemiesBuilder();
+    });
+  // .catch((error) => console.log(error));
+};
+
 const enemiesBuilder = () => {
   // this function should display the array of enemies onto cards in the DOM
   enemyData.getAllEnemies()
     .then((enemies) => {
     // console.log('the enemies', enemies);
-      let domString = '';
+      let domString = `<button type="button" id="addNewEnemyBtn" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+      Add Enemy
+    </button>`;
       enemies.forEach((enemy) => {
         domString += enemyCard.makeEnemyCard(enemy);
       });
       utilities.printToDom('enemiesPage', domString);
+      $('#addNewEnemyBtn').click(addNewEnemy);
     });
   // .catch((error) => console.error(error));
 };
