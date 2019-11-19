5f9d0b1c8f6b71c524fd78d7f06cefc4931a829f
John Thielman <john.thielman@gmail.com>
2019-11-18 22:02:53 -0600

    WIP

--- src/javascripts/components/Weapons/weapons.js
+++ src/javascripts/components/Weapons/weapons.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
-// import firebase from 'firebase/app';
-// import 'firebase/auth';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import weaponsData from '../../helpers/data/weaponsData';
 
@@ -16,12 +16,34 @@ const showDeets = (e) => {
   $(`#${clickedWeapon}-full-img`).addClass('hide');
 };
 
+const makeNewWeapon = (e) => {
+  e.stopImmediatePropagation();
+  const newWeapon = {
+    name: $('#weapon-name').val(),
+    isActive: $('#weapon-status').val(),
+    teamSize: $('#team-size').val() * 1,
+    type: $('weapon-use').val(),
+    img: $('#weapon-image-url').val(),
+  };
+  weaponsData.addNewWeapon(newWeapon)
+    .then(() => {
+      $('#weaponsModal').modal('hide');
+      // eslint-disable-next-line no-use-before-define
+      showTheWeapons(e);
+    })
+    .catch((error) => console.error(error));
+};
+
 const showTheWeapons = (e) => {
   e.stopImmediatePropagation();
+  const user = firebase.auth().currentUser;
   $('#dashboard').addClass('hide');
   weaponsData.getWeapons()
     .then((weppens) => {
       let domString = '<h1>Armory</h1>';
+      if (user) {
+        domString += '<button class="btn btn-outline-light" data-toggle="modal" data-target="#weaponsModal">Add Weapon</button>';
+      }
       domString += '<div class="row"><div class="card-group">';
       weppens.forEach((weppen) => {
         domString += `
@@ -48,8 +70,51 @@ const showTheWeapons = (e) => {
         `;
       });
       domString += '</div></div>';
+      domString += `
+      <div class="modal fade" id="weaponsModal" tabindex="-1" role="dialog" aria-labelledby="weaponsModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="weaponsModalLabel">New Weapon</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="weapon-name">Name</label>
+                  <input type="text" class="form-control" id="weapon-name" placeholder="Enter Name">
+                </div>
+                <div class="form-group">
+                  <label for="weapon-status">Status</label>
+                  <input type="number" class="form-control" id="weapon-status" placeholder="Enter Status">
+                </div>
+                <div class="form-group">
+                  <label for="team-size">Crew</label>
+                  <input type="number" class="form-control" id="team-size" placeholder="Enter Crew Size">
+                </div>
+                <div class="form-group">
+                  <label for="weapon-use">Use</label>
+                  <input type="number" class="form-control" id="weapon-use" placeholder="Enter Status">
+                </div>
+                <div class="form-group">
+                  <label for="weapon-image-url">Image Url</label>
+                  <input type="text" class="form-control" id="weapon-image-url" placeholder="Enter image Url">
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary" id="add-weapon-btn">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      `;
       utilities.printToDom('weaponsPage', domString);
       $('#weaponsPage').on('click', '.card-img', showDeets);
+      $('#weaponsPage').on('click', '#add-weapon-btn', makeNewWeapon);
     })
     .catch((error) => console.error(error));
 };
@@ -58,4 +123,4 @@ const clickWeapons = () => {
   $('#weaponsLink').click(showTheWeapons);
 };
 
-export default { clickWeapons };
+export default { clickWeapons, makeNewWeapon };
