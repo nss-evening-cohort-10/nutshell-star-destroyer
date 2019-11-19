3878e0893050cebb6c08f5d573322b3da7cb8ac1
mariabrock <mariabrock414@gmail.com>
2019-11-18 19:51:03 -0600

    wip

--- src/javascripts/helpers/utilities.js
+++ src/javascripts/helpers/utilities.js
@@ -3,7 +3,6 @@ import $ from 'jquery';
 const printToDom = (divId, textToPrint) => {
   const selectedDiv = $(`${divId}`);
   selectedDiv.html(textToPrint);
-  console.log(selectedDiv[0]);
 };
 
 export default { printToDom };
