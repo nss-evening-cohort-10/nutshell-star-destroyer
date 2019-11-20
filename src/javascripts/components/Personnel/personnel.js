47ee533d55ecb1e0056036a5fc57bb15d87d6571
Emilee Mitchell <emcakes@gmail.com>
2019-11-19 19:10:45 -0600

    WIP

--- src/javascripts/components/Personnel/personnel.js
+++ src/javascripts/components/Personnel/personnel.js
@@ -10,7 +10,6 @@ const addNewPersonnel = () => {
     sectorId: $('#sector-id').val(),
     weaponId: $('#weapon-id').val(),
   };
-  console.log(newPersonnel);
   personnelData.CreateNewPersonnel(newPersonnel)
     .then(() => {
       $('#exampleModalCenter').modal('hide');
@@ -24,16 +23,8 @@ const clickAddNew = () => {
   $('#add-new-personnel').click(addNewPersonnel);
 };
 
-const updatePersonnel = (e) => {
-  $('#exampleModalCenter').modal('show');
-  const personId = e.target.id.split('edit-personnel--')[1];
-  personnelData.getPersonnelById(personId);
-    // eslint-disable-next-line no-use-before-define
-  .then(() => displayCrew())
-  .catch((error) => console.error(error));
-};
-
 const personnelEventListeners = () => {
+  // eslint-disable-next-line no-use-before-define
   $('#personnel').on('click', '.edit', updatePersonnel);
   // $('#personnel').on('click', '.delete', deletePersonnel);
 };
@@ -46,59 +37,58 @@ const displayCrew = (e) => {
       domString += '<button id="add-personnel" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Add New Personnel</button>';
       domString += `<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
+      <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalCenterTitle">Add New Personnel</h5>
+          <h5 class="modal-title" id="exampleModalCenterTitle">Add New Personnel</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
+            <span aria-hidden="true">&times;</span>
             </button>
-          </div>
-          <div class="modal-body">
+            </div>
+            <div class="modal-body">
           <form id="Add New Personnel">
           <div class="form-group">
           <label for="name-id">Name</label>
-          <input type="text" class="form-control" id="name-id" placeholder="Enter Name">
-        </div>
+          <input type="text" class="form-control" id="name-id" value="${personnel.name ? personnel.name : ''}" placeholder="Enter Name">
+          </div>
           <div class="form-group">
             <label for="sector-id">Sector</label>
             <input type="text" class="form-control" id="sector-id" placeholder="Enter Sector">
-          </div>
+            </div>
           <div class="form-group">
             <label for="weapon-id">Weapon</label>
             <input type="text" class="form-control" id="weapon-id" placeholder="Enter Weapon">
           </div>
           <div class="form-group">
-            <label for="image-url">Image</label>
+          <label for="image-url">Image</label>
             <input type="text" class="form-control" id="image-url" placeholder="Enter Image">
           </div>
-        </form>
+          </form>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
             <button type="button" class="btn btn-primary" id="add-new-personnel">Save changes</button>
           </div>
         </div>
-      </div>
-    </div>`;
+        </div>
+        </div>`;
       domString += '<div class="row">';
       personnel.forEach((person) => {
         domString += `
         <div class="col-md-6">
           <div class="card">
           <img src="${person.personImg}" class="card-img-top" alt="...">
-            <div class="card-body">
+          <div class="card-body">
               <h5 class="card-title"> ${person.name}</h5>
               <p class="card-text">Sector: ${person.sectorId}</p>
               <p class="card-text">Weapon: ${person.weaponId}</p>
               <button id="edit-personnel-${person.id}" class="btn btn-primary edit">Edit</button>
               <button id="delete-personnel-${person.id}" class="btn btn-primary delete">Delete</button>
-            </div>
-          </div>
+              </div>
+              </div>
         </div>
         `;
       });
       domString += '</div>';
-      // console.log(domString);
       utilities.printToDom('personnel', domString);
       clickAddNew();
       personnelEventListeners();
@@ -106,8 +96,20 @@ const displayCrew = (e) => {
     .catch((error) => console.error(error));
 };
 
+const updatePersonnel = (e) => {
+  $('#exampleModalCenter').modal('show');
+  const personId = e.target.id.split('edit-personnel-')[1];
+  console.log(personId);
+  personnelData.getPersonnelById(personId)
+  // eslint-disable-next-line no-use-before-define
+  // ??????????????????????????????????
+    .then((person) => {
+      displayCrew();
+    })
+    .catch((error) => console.error(error));
+};
+
 const clickCrew = () => {
-  // $('#person-button').click(console.log('tacos'));
   $('#personnelLink').click(displayCrew);
 };
 
