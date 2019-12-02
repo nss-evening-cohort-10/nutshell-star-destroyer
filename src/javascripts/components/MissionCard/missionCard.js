d3a2c86207a94f80b132af1f435ac462ee5323c6
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-12-01 20:11:06 -0600

    WIP

--- src/javascripts/components/MissionCard/missionCard.js
+++ src/javascripts/components/MissionCard/missionCard.js
@@ -4,7 +4,8 @@ import firebase from 'firebase';
 const makeMissionCard = (mission) => {
   let domString = '';
   const user = firebase.auth().currentUser;
-  domString += `<div class="card mission">
+  domString += `
+  <div class="card mission" id="${mission.id}">
   <div class="card-body text-center" ${mission.id}>
   <h1 class="card-title">Mission:${mission.missionTitle}</h1>
   <p class="card-text">Enenmy Target: ${mission.enemyID}</p>
@@ -13,8 +14,8 @@ const makeMissionCard = (mission) => {
   <div>`;
   if (user != null) {
     domString += `
-        <button href="#" id="${mission.id}" class="btn btn-danger deleteEnemy">Delete</button>
-        <button href="#" id="${mission.id}" class="btn btn-secondary editEnemy" type="button" data-toggle="modal" data-target="#newMissionModal">Edit</button>`;
+        <button id="${mission.id}" class="btn btn-danger deleteMission">Delete</button>
+        <button id="${mission.id}" class="btn btn-secondary editMission" type="button" data-toggle="modal" data-target="#newMissionModal">Edit</button>`;
     domString += '</div>';
   }
   return domString;
