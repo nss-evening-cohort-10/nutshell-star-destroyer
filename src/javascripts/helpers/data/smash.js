640bd0416c16ee62de696cfbea228d169a92dd9a
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-12-02 23:11:51 -0600

    WIP

---
+++ src/javascripts/helpers/data/smash.js
@@ -0,0 +1,16 @@
+import missionData from './missionData';
+import enemyData from './enemyData';
+import missionWeaponsData from './missionWeaponsData';
+import planetSystemData from './planetSystemData';
+
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+import 'firebase/auth';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+
+const getCompleteMission = (missionId) => new Promise((resolve, reject) => { 
+  missionData.getMissionById(missionId)
+  .then((mission) => enemyData.getEnemyById() 
+});
