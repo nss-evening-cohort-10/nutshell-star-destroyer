2eff8e0196bcd0e148664b671984f943f2c6827c
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-12-04 09:20:44 -0600

    test to fix weapons update

--- src/javascripts/helpers/data/smash.js
+++ src/javascripts/helpers/data/smash.js
@@ -1,5 +1,5 @@
-// import missionData from './missionData';
-// import enemyData from './enemyData';
+import missionData from './missionData';
+import enemyData from './enemyData';
 // import missionWeaponsData from './missionWeaponsData';
 // import planetSystemData from './planetSystemData';
 
@@ -9,7 +9,18 @@
 
 // const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
-// // const getCompleteMission = (missionId) => new Promise((resolve, reject) => {
-// //   missionData.getMissionById(missionId)
-// //   .then((mission) => enemyData.getEnemyById()
-// // }):
+const getCompleteMission = (missionId) => new Promise((resolve, reject) => {
+  const missionObj = {};
+  missionData.getMissionById(missionId)
+    .then((mission) => {
+      console.log(mission);
+      missionObj.missionTitle = mission.missionTitle;
+      enemyData.getEnemyById(mission.enemyID)
+        .then((enemy) => {
+          console.log('test', enemy);
+          missionObj.enemyID = enemy.id;
+        })
+        .catch((error) => reject(error));
+    });
+});
+export default { getCompleteMission };
