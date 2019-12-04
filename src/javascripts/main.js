ccbe60f7736bd8dbf65e47ad37ced9a77c6f6f69
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-12-03 19:14:52 -0600

    whatev

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,15 +4,12 @@ import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys';
 import authData from './helpers/data/authData';
 import ourNavbar from './components/OurNavbar/ourNavbar';
-import personnelDisplay from './components/Personnel/personnel';
-import enemies from './components/Enemies/enemies';
+
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   authData.checkLoginStatus();
   ourNavbar.logoutEvent();
-  enemies.clickForEnemies();
-  personnelDisplay.clickCrew();
 };
 
 init();
